1bf4c7eeddb9dee71ecd5aa4c938a8beadde5239
Alvin <>
2019-12-31 17:21:59 +0800

    補上108-1final exam 單學生比較

--- codeView/f2.py
+++ codeView/f2.py
@@ -1,2 +1,17 @@
-name = input("WHAT's your name?")
-Print("Hello,"+name+" ".")
+a = list(map(int, input().split()))
+
+for i in range(len(a)):
+    for j in range(i):
+        if a[j] > a[j+1]:
+            temp = a[j]
+            a[j] = a[j+1]
+            a[j+1] = temp
+
+b = {}
+
+for i in set(a):
+    b[i] = a.count(i)
+
+for key, value in b.items():
+    print(str(key) + ":" + str(value), end = " ")
+    print()
